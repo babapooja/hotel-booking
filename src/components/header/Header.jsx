dacf1b4070ec89b40659ac219c7035af29507963
Pooja Baba <babapooja6@gmail.com>
2023-05-20 18:15:04 -0400

    completed header section added advanced search and its related components such as the calendar to select the date ranges, options dropdown box

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,63 @@
-import { faBed, faPlane, faCar, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import { faBed, faPlane, faCar, faTaxi, faCalendarDays, faPerson } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import { format } from "date-fns";
 
-const Header = () => {
+const Header = ({ type }) => {
+    const optionValues = ['Adult', 'Children', 'Room'];
+    const [openDate, setOpenDate] = useState(false);
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ]);
+
+    const [openOptions, setOpenOptions] = useState(false);
+    const [options, setOptions] = useState({
+        adult: 1,
+        children: 0,
+        room: 0
+    })
+
+    const handleOption = (type, iord) => {
+        setOptions(prev => {
+            return {
+                ...prev,
+                [type]: iord == '-' ? options[type] - 1 : options[type] + 1
+            }
+        })
+    }
+
+    const displayOptions = () => {
+        return <div className="options">
+            {
+                optionValues.map(optionValue => {
+                    return <div className="optionItem">
+                        {/* adult */}
+                        <span className="optionText">{optionValue}</span>
+                        <div className="optionCounter">
+                            <button className="optionCounterButton" disabled={options[optionValue.toLowerCase()] < 1} onClick={() => handleOption(optionValue.toLowerCase(), "-")}>-</button>
+                            <span className="optionCounterNumber">{options[optionValue.toLowerCase()]}</span>
+                            <button className="optionCounterButton" onClick={() => handleOption(optionValue.toLowerCase(), "+")}>+</button>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
+
+
+
+
+    }
+
+
     return (
         <div className="header">
-            <div className="headerContainer">
+            <div className={type == "list" ? "headerContainer listMode" : "headerContainer"}>
                 <div className="headerList">
                     <div className="headerListItem active">
                         <FontAwesomeIcon icon={faBed} />
@@ -28,15 +80,48 @@ const Header = () => {
                         <span>Airport Taxis</span>
                     </div>
                 </div>
-                <h1 className="headerTitle">
-                    A lifetime of discounts? It's Genius
-                </h1>
-                <p className="headerDesc">
-                    Get rewarded for your travels - unlock instant savings of 10% or more with a free account.
-                </p>
-                <button className="headerBtn">
-                    Sign In/ Register
-                </button>
+                {
+                    type != "list" && <>
+                        <h1 className="headerTitle">
+                            A lifetime of discounts? It's Genius
+                        </h1>
+                        <p className="headerDesc">
+                            Get rewarded for your travels - unlock instant savings of 10% or more with a free account.
+                        </p>
+                        <button className="headerBtn">
+                            Sign In/ Register
+                        </button>
+                        <div className="headerSearch">
+                            {/* search text input */}
+                            <div className="headerSearchItem">
+                                <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                                <input type="text" placeholder="Where are you going?" className="headerSearchInput" />
+                            </div>
+                            {/* calendar */}
+                            <div className="headerSearchItem">
+                                <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                                <span onClick={() => setOpenDate(!openDate)} className="headerSearchText">{`${format(date[0].startDate, "MM/dd/yyyy")} to  ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                                {openDate && <DateRange
+                                    ranges={date}
+                                    onChange={item => setDate([item.selection])}
+                                    editableDateInputs={true}
+                                    moveRangeOnFirstSelection={false}
+                                    className="date"
+                                />}
+
+                            </div>
+                            {/* selecting the options */}
+                            <div className="headerSearchItem">
+                                <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                                <span onClick={() => { setOpenOptions(!openOptions) }} className="headerSearchText">{`${options.adult} adults . ${options.children} children . ${options.room} room `}</span>
+                                {openOptions && displayOptions()}
+                            </div>
+                            <div className="headerSearchItem">
+                                <button className="headerBtn">Search</button>
+                            </div>
+                        </div>
+                    </>
+                }
             </div>
         </div>
     )
